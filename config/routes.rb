58832c46bfa47c2a3a1eb4d11ee77ddb91eae329
EC2 Default User <abcdquont1234@gmail.com>
2021-01-20 14:31:08 +0000

    user,post controller追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'posts/index'
+  get 'posts/show'
+  get 'users/index'
+  get 'users/show'
   get 'welcome/index'
 
   resources :articles
@@ -6,6 +10,11 @@ Rails.application.routes.draw do
   resources :comments
 
   resources :favorites, only: [:create, :destroy]
+
+  resources :users, only: [:index, :show]
+  resources :posts, only: [:index, :show, :create]
+
+  root 'posts#index'
 
   root 'welcome#index'
 end
