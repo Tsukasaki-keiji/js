b72a0871526d69abce1011321d20eeec78e35a51
EC2 Default User <abcdquont1234@gmail.com>
2021-01-25 14:01:34 +0000

    favorites変更

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,17 +1,30 @@
 class FavoritesController < ApplicationController
 
-  def create
-    post_image = PostImage.find(params[:post_image_id])
-    favorite = current_user.favorites.new(post_image_id: post_image.id)
-    favorite.save
-    redirect_to post_image_path(post_image)
+  before_action :authenticate_user!, only: [:show, :create]
+  def index
+    @posts = Post.all
+    @post = Post.new
   end
 
-  def destroy
-    post_image = PostImage.find(params[:post_image_id])
-    favorite = current_user.favorites.find_by(post_image_id: post_image.id)
-    favorite.destroy
-    redirect_to post_image_path(post_image)
+  def show
+    @post = Post.find(params[:id])
+    @like = Like.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.user_id = current_user.id
+    if @post.save
+      redirect_back(fallback_location: root_path)
+    else
+      redirect_back(fallback_location: root_path)
+    end
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:content)
+  end
   end
 
 end
