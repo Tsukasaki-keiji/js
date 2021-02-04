3aaf84fb6197e2a30148e28f1b29997d4eabe1f8
EC2 Default User <abcdquont1234@gmail.com>
2021-02-04 14:08:06 +0000

    ransack導入 試し

--- Gemfile
+++ Gemfile
@@ -64,3 +64,5 @@ end
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 gem 'acts-as-taggable-on', '~> 6.0'
+
+gem 'ransack'
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+
+  before_action :set_search
+
+    def set_search
+      @search = Product.ransack(params[:q]) #ransackの検索メソッド
+      @search_products = @search.result(distinct: true).order(created_at: "DESC").includes(:user).page(params[:page]).per(5)
+    end
 end
